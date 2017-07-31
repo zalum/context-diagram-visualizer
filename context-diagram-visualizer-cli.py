06b3cd223072865cdaec6ae824d4da6796c3d161
florin <Florin.Zalum@zooplus.com>
2017-07-31 14:32:32 +0200

    call plantuml to generate the picture

--- context-diagram-visualizer-cli.py
+++ context-diagram-visualizer-cli.py
@@ -6,3 +6,4 @@ import graph_visualizer
 inputSystemGraph = system_graph.SystemGraph(graph_input.getSystemGraph())
 lines = graph_visualizer.GraphVisualizer(inputSystemGraph).draw()
 graph_output.writeAsText(lines)
+graph_output.writeAsImage(lines)
--- graph_output.py
+++ graph_output.py
@@ -1,7 +1,17 @@
+import subprocess
+
 def writeAsText(lines):
     f = open('output.plantuml', 'w')
-    for line in lines:
-        f.write(line+"\n")
+    content = _prepare_content(lines)
+    f.write(content)
+
 
 def writeAsImage(lines):
-    #cat test.txt|java -jar plantuml.1.2017.15.jar -pipe > somefile.png
+    f = open('output.jpg', 'w')
+    p = subprocess.Popen(["java","-jar","/data/tools/plant-uml/plantuml.1.2017.15.jar","-pipe"],stdin=subprocess.PIPE,stdout = f)
+    content = _prepare_content(lines)
+    result = p.communicate(bytes(content,"UTF-8"))
+
+
+def _prepare_content(lines):
+    return "\n".join(lines)
